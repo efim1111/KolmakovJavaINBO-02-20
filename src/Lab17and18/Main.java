9ac6ffd206e7d932640c1910429c8467333e5a9b
Efim Kolmakov <efim.kolmakov1703@gmail.com>
2021-11-14 18:14:47 +0300

    Добавил 17-18

---
+++ src/Lab17and18/Main.java
@@ -0,0 +1,15 @@
+package Lab17and18;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String input = "abcdefghijklmnopqrstuv18340";
+        System.out.println(input);
+        boolean result = input.matches("abcdefghijklmnopqrstuv18340");
+        if (result) {
+            System.out.println("Данная строка является строкой \"abcdefghijklmnopqrstuv18340\"\n");
+        } else {
+            System.out.println("Данная строка НЕ является строкой \"abcdefghijklmnopqrstuv18340\"\n");
+        }
+    }
+}
