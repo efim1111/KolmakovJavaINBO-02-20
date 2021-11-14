057feb0a657e8ad50522146839e02a29a85e3bfe
Efim Kolmakov <efim.kolmakov1703@gmail.com>
2021-11-14 17:56:44 +0300

    Добавил 16

---
+++ src/Lab16/Item.java
@@ -0,0 +1,10 @@
+package Lab16;
+
+public interface Item {
+    int getCost();
+    void setCost(int cost);
+    String getName();
+    void setName(String name);
+    Dish getDescription();
+    void setDescription(Dish description);
+}
---
+++ src/Lab16/Order.java
@@ -0,0 +1,13 @@
+package Lab16;
+
+interface Order {
+    boolean addPosition(Item a);
+    String[] names();
+    int count();
+    int count(String itemName);
+    Item[] getItems();
+    boolean delete(String itemName);
+    int deleteAll(String itemName);
+    Item[] sorted();
+    int cost();
+}
